5e03e774b142d4a52a8ffbc52c210b577d6dfcca
Luis Vice <khanccer@gmail.com>
2020-10-25 23:33:33 -0600

    version 4

--- lib/bloc/usuario/usuario_bloc.dart
+++ lib/bloc/usuario/usuario_bloc.dart
@@ -1,6 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:bloc/bloc.dart';
+
 import 'package:estados_cubit/models/usuario.dart';
 
 part 'usuario_state.dart';
 part 'usuario_event.dart';
 
-class UsuerioBloc {}
+class UsuarioBloc extends Bloc<UsuarioEvent, UsuarioState> {
+  UsuarioBloc() : super(UsuarioState());
+
+  @override
+  Stream<UsuarioState> mapEventToState(UsuarioEvent event) {
+    print('Hola Mundo');
+  }
+}
